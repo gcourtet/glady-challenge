419b3186ee2f738d96c35b32ba6898e60e760a6e
Guillaume Courtet <guillaume.courtet@hotmail.fr>
2022-06-19 16:13:29 +0200

    :tada: Initial commit

---
+++ src/main/java/com/gcourtet/glady/challenge/GladyChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gcourtet.glady.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GladyChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GladyChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcourtet/glady/challenge/GladyChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcourtet.glady.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GladyChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
